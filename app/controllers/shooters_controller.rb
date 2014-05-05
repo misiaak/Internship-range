84ab9abda9cc5c4301db7e18aed4e209fceb13ab
Marta Buda <bmarta1991@o2.pl>
2014-05-05 16:04:58 +0200

    49-create-shooters_controller
    [#49]add-shooters_controller
    
    [#49]shooters-controller modification

---
+++ app/controllers/shooters_controller.rb
@@ -0,0 +1,18 @@
+class ShootersController < ApplicationController
+
+  #list of all shooters
+  def index
+  end
+
+  #show details of choosen shooter
+  def show
+  end
+  
+  #add new shooter
+  def create
+  end
+  
+  #add payment for shooter
+  def add_payment
+  end
+end
