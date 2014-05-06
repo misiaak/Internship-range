a18bd0df109e7a0898de22c5c8eb620ca3a06d5d
Mateusz Nowakowski <nowakov@gmail.com>
2014-05-06 12:48:10 +0200

    [#38] - Create model payment
    Minor tab fix

---
+++ app/models/payment.rb
@@ -0,0 +1,13 @@
+class Payment < ActiveRecord::Base
+	validates :description, presence: true
+	validates :amount, presence: true
+	validates :user_id, presence: true
+	validate :date_is_date?
+
+	private
+	def date_is_date?
+		unless date.is_a?(Date)
+			errors.add(:date, 'must be a valid date')
+		end
+	end
+end
